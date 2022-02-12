3482be88d43e49e0305b73066fd55d1e512d367b
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-02-12 22:01:29 +0800

    - Update Scene Level 1-1
    - Added Camera prefab

--- Agent Satu [GPS1_Project]/Assets/Scripts/PlayerControls/Cursor/CameraTarget.cs
+++ Agent Satu [GPS1_Project]/Assets/Scripts/PlayerControls/Cursor/CameraTarget.cs
@@ -5,17 +5,17 @@ public class CameraTarget : MonoBehaviour
 {
     //Components
     public CrosshairAiming aim;
-    public Transform player;
+    public Transform playerBody;
     
     //Field
     [SerializeField] private float threshold;
     
     void FixedUpdate()
     {
-        Vector2 targetPos = (Vector2) player.position + aim.GetMousePos();
+        Vector2 targetPos = (Vector2) playerBody.position + aim.GetMousePos();
 
-        targetPos.x = Mathf.Clamp(targetPos.x, -threshold + player.position.x, threshold + player.position.x);
-        targetPos.y = Mathf.Clamp(targetPos.y, -threshold + player.position.y, threshold + player.position.y);
+        targetPos.x = Mathf.Clamp(targetPos.x, -threshold + playerBody.position.x, threshold + playerBody.position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, -threshold + playerBody.position.y, threshold + playerBody.position.y);
 
         transform.position = targetPos;
     }
