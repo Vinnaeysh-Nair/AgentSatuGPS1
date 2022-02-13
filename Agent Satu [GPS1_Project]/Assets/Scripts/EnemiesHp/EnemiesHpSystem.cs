0d9ee9faf874bec724afad9555cc97853714f5a3
JakeCJC <98402268+JakeCJC@users.noreply.github.com>
2022-02-13 22:56:55 +0800

    Enemies hp system
    added to death trigger for enemies

---
+++ Agent Satu [GPS1_Project]/Assets/Scripts/EnemiesHp/EnemiesHpSystem.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemiesHpSystem : MonoBehaviour
+{
+    private int hpCount;
+    public GameObject bulletDetect;
+    public GameObject enemiesToDestroy;
+    void Start()
+    {
+        hpCount = 5;
+        Debug.Log("Start debug");
+    }
+
+    void Update()
+    {
+        if (hpCount == 0)
+        {
+            DestroyImmediate(enemiesToDestroy, true);
+        }
+    }
+
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnTriggerEnter2D(Collider2D bulletDetect)
+    {
+        Debug.Log("hit");
+        hpCount -= 1;
+
+        Debug.Log(hpCount);
+    }
+}
