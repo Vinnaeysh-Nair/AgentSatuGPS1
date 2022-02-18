3005fdde989eb8f1a4525ade198ec862405f95b5
JakeCJC <98402268+JakeCJC@users.noreply.github.com>
2022-02-18 14:59:38 +0800

    Made temporary scene thingy
    work in progress

---
+++ Agent Satu [GPS1_Project]/Assets/Scripts/SceneTransition/sceneTransition.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using unityEngine.scene
+
+public class sceneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
