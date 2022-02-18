38553e77931a3cf4bdad73e060c831a9cc0df170
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-02-18 22:14:59 +0800

    updated prefab

--- Agent Satu [GPS1_Project]/Assets/SpawnPickups.cs
+++ Agent Satu [GPS1_Project]/Assets/SpawnPickups.cs
@@ -4,7 +4,7 @@ public class SpawnPickups : MonoBehaviour
 {
     //Components
     [SerializeField] Transform[] picksUpSpawnedWhenDead = new Transform[2];
-    [SerializeField] [Range(0f, 1f)] private float bulletSpawnRate = 0.6f;
+    [SerializeField] [Range(0f, 1f)] private float bulletPickUpSpawnRate = 0.6f;
     [SerializeField] private float spawnForce = 1f;
 
     private ObjectPooler pooler;
@@ -26,16 +26,16 @@ public class SpawnPickups : MonoBehaviour
         float rng = Random.Range(0f, 1f);
         int itemIndex = 0;
 
-        if (rng > bulletSpawnRate)
+        if (rng > bulletPickUpSpawnRate)
         { 
             //Spawn health
-            Debug.Log("spawned health");
+            //Debug.Log("spawned health");
             itemIndex = 1;
         }
         else
         {
             //Spawn bullet pickup
-            Debug.Log("spawned bullet pickup");
+            //Debug.Log("spawned bullet pickup");
         }
        
         Debug.Log(transform.position);
