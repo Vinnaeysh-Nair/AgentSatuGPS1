2c2c0dc18bec2431ada07cac14177031a85aa15a
JakeCJC <98402268+JakeCJC@users.noreply.github.com>
2022-02-19 15:28:02 +0800

    create scene transition
    still testing

---
+++ Agent Satu [GPS1_Project]/Assets/Scenes/WinLose/TransitionScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScript : MonoBehaviour
+{
+    public KeyCode transitionButton;
+    public Animator transition;
+    public float transitionTime = 1f;
+    public GameObject entranceCollider;
+    void Update()
+    {
+        
+        
+        if (Input.GetKeyDown(transitionButton))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(entranceCollider==collision)
+            Debug.Log("Entrance triggered");
+        //LoadNextLevel();
+    }
+}
