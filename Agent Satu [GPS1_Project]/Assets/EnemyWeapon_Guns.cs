8ce79fb61308be26091dae82eb57cb6554ce36f6
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-02-20 00:01:24 +0800

    made prefabs for enemy weapons

--- Agent Satu [GPS1_Project]/Assets/EnemyWeapon_Guns.cs
+++ Agent Satu [GPS1_Project]/Assets/EnemyWeapon_Guns.cs
@@ -1,20 +1,80 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class EnemyWeapon_Guns : MonoBehaviour
 {
     //Components
     private ObjectPooler pooler;
+    public GameObject bullet;
 
+
+    //Fields
+    [SerializeField] private float fireRate = 1f;
+    [SerializeField] private bool isBurst = false;
     
+    [Header("For burst weapons(tick isBurst for stats below to apply)")]
+    [SerializeField] private float burstInterval = 5f;
+    [SerializeField] private int bulletPerBurst;
 
+    private Transform[] firePoints;
+    private float nextFireTime = 0f;
+    private bool canStartBurstShooting = true;
+    private int shotBullet = 0;
+    
     void Awake()
     {
         pooler = ObjectPooler.objPoolerInstance;
     }
+    
+    void Start()
+    {
+        int size = transform.childCount;
+        firePoints = new Transform[size];
+        for (int i = 0; i < size; i++)
+        {
+            firePoints[i] = transform.GetChild(i);
+        }
+    }
+
+    
+    //Call from enemy AI script, when player is detected
+    //For single shot firing pattern (eg. pistol)
+    public void StartShooting()
+    {
+        if (Time.time > nextFireTime)
+        {
+            Shoot(bullet, firePoints);
+            nextFireTime = Time.time + (1f / fireRate);
+        }
+    }
+    
+    //For burst firing pattern (eg. SMG)
+    public void StartBurstShooting(int bulletPerBurst)
+    {
+        if (canStartBurstShooting && Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + (1f / fireRate);
+            Shoot(bullet, firePoints);
+            shotBullet++;
+        }
+
+        if (shotBullet == bulletPerBurst)
+        {
+            StartCoroutine(SetCanStartBurstShootingToTrue());
+            canStartBurstShooting = false;
+            shotBullet = 0;
+        }
+    }
 
 
-    public void Shoot(GameObject bullet, Transform[] firePoints)
+    private IEnumerator SetCanStartBurstShootingToTrue()
+    {
+        yield return new WaitForSeconds(burstInterval);
+        canStartBurstShooting = true;
+    }
+    
+    private void Shoot(GameObject bullet, Transform[] firePoints)
     {
         foreach (Transform firePoint in firePoints)
         {
@@ -23,16 +83,6 @@ public class EnemyWeapon_Guns : MonoBehaviour
         }
     }
 
-    public IEnumerator BurstShoot(GameObject bullet, Transform[] firePoints, float fireRate)
-    {
-        yield return new WaitForSeconds(fireRate);
-        
-        foreach (Transform firePoint in firePoints)
-        {
-            GameObject shotBullet = pooler.SpawnFromPool(bullet.name, firePoint.position, firePoint.rotation);
-            StartCoroutine(SetBulletInactive(shotBullet));
-        }
-    }
     
     //Become inactive after a duration after being fired. 
     private IEnumerator SetBulletInactive(GameObject shotBullet)
@@ -40,4 +90,5 @@ public class EnemyWeapon_Guns : MonoBehaviour
         yield return new WaitForSeconds(1f);
         shotBullet.SetActive(false);
     }
+
 }
