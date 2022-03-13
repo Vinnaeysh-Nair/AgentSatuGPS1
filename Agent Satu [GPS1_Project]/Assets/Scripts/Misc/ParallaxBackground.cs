effd6f76038e5fdb6354cbc5a160c3777d95400d
JakeCJC <98402268+JakeCJC@users.noreply.github.com>
2022-03-14 02:19:35 +0800

    added infinite background

---
+++ Agent Satu [GPS1_Project]/Assets/Scripts/Misc/ParallaxBackground.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public RawImage backgroundPicture;
+    public float height;
+    public float length;
+
+    void Update()
+    {
+        backgroundPicture.uvRect = new Rect(backgroundPicture.uvRect.position + new Vector2(length, height) * Time.deltaTime, backgroundPicture.uvRect.size);
+    }
+}
