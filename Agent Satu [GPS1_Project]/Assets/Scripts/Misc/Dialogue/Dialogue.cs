01f21eec102ba09650817bfbd16b98eba7602ea0
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-03-20 23:10:15 +0800

    dialogue text box adjustments

--- Agent Satu [GPS1_Project]/Assets/Scripts/Misc/Dialogue/Dialogue.cs
+++ Agent Satu [GPS1_Project]/Assets/Scripts/Misc/Dialogue/Dialogue.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue
 {
+    public int speakerId;
     public string name;
-
+    
     [TextArea(3, 10)]
     public string[] sentences;
 }
--- Agent Satu [GPS1_Project]/Assets/Scripts/Misc/Dialogue/DialogueManager.cs
+++ Agent Satu [GPS1_Project]/Assets/Scripts/Misc/Dialogue/DialogueManager.cs
@@ -5,17 +5,43 @@ public class DialogueManager : MonoBehaviour
 {
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI dialogueText;
- 
+
+
+    public TextColor[] textColors;
+    
+    [System.Serializable]
+    public class TextColor
+    {
+        public int speakerId;
+        public Color textColor;
+    }
     
     public void StartDialogue(Dialogue dialogue)
     {
         nameText.text = dialogue.name;
+
+        foreach (TextColor txtColor in textColors)
+        {
+            if (txtColor.speakerId == dialogue.speakerId)
+            { 
+                nameText.color = txtColor.textColor;
+            }
+        }
     }
     
-    public void DisplayNextSentence(string sentence)
+    public void DisplayNextSentence(Dialogue dialogue, int sentenceIndex)
     {
-        dialogueText.text = sentence;
+        dialogueText.text = dialogue.sentences[sentenceIndex];
+
+        foreach (TextColor txtColor in textColors)
+        {
+            if (txtColor.speakerId == dialogue.speakerId)
+            {
+                dialogueText.color = txtColor.textColor;
+            }
+        }
     }
+
 
     void EndDialogue()
     {
