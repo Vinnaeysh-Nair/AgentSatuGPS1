6791442bf1d886036df8d11342ea2867f14b9163
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-03-20 18:12:33 +0800

    adjusted 1-2

--- Agent Satu [GPS1_Project]/Assets/Scripts/Misc/Dialogue/DialogueAndLines.cs
+++ Agent Satu [GPS1_Project]/Assets/Scripts/Misc/Dialogue/DialogueAndLines.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class DialogueAndLines : MonoBehaviour
 {
-    public Dialogue dialogue;
+    public Dialogue[] dialogue;
+    private int currDialogue = 0;
+    private int currSentence;
 
     //private bool canTalk = true;
     //private float timer = 0.5f;
@@ -29,7 +31,19 @@ public class DialogueAndLines : MonoBehaviour
             //canTalk &&
             if (Input.GetKeyDown("e"))
             {
-                TriggerNextDialogue();
+                if (!IsALLDialoguesFinished())
+                {
+                    if (IsAllSentencesFinished())
+                    {
+                        currSentence = 0;
+                    }
+                    TriggerNextDialogue();
+                }
+                else
+                {
+                    print("no more");
+                }
+  
                 //canTalk = false;
             }
         }
@@ -43,12 +57,24 @@ public class DialogueAndLines : MonoBehaviour
 
     public void TriggerDialogue()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue[currDialogue]);
+        currDialogue++;
     }
 
     public void TriggerNextDialogue()
     {
         FindObjectOfType<DialogueManager>().DisplayNextSentence();
+        currDialogue++;
+    }
+
+    private bool IsAllSentencesFinished()
+    {
+        return currSentence > dialogue[currDialogue].sentences.Length - 1;
+    }
+
+    private bool IsALLDialoguesFinished()
+    {
+        return currDialogue > dialogue.Length - 1;
     }
 
     void OnDisable ()
