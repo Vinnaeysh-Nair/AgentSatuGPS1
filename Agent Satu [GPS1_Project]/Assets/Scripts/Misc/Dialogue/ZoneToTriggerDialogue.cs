211a16b2f45a6fd6b86b105b6945cf471c464be3
JakeCJC <98402268+JakeCJC@users.noreply.github.com>
2022-03-20 17:22:04 +0800

    made some changes to dialogue

--- Agent Satu [GPS1_Project]/Assets/Scripts/Misc/Dialogue/ZoneToTriggerDialogue.cs
+++ Agent Satu [GPS1_Project]/Assets/Scripts/Misc/Dialogue/ZoneToTriggerDialogue.cs
@@ -17,8 +17,8 @@ public class ZoneToTriggerDialogue : MonoBehaviour
     {
         if(inTalkingZone)
         {
-            if (Input.GetKey("p"))
-                TriggeringDialogue();
+            //if (Input.GetKey("p"))//use this for dialogue trigger by button
+            TriggeringDialogue();
         }
         else
         {
