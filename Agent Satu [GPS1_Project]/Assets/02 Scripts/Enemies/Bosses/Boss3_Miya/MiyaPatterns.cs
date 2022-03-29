74368fab92dcc4d5e2529bb904f83c4bbcb9dd44
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-03-29 14:07:01 +0800

    miya

--- Agent Satu [GPS1_Project]/Assets/02 Scripts/Enemies/Bosses/Boss3_Miya/MiyaPatterns.cs
+++ Agent Satu [GPS1_Project]/Assets/02 Scripts/Enemies/Bosses/Boss3_Miya/MiyaPatterns.cs
@@ -3,10 +3,25 @@ using System.Collections;
 
 public class MiyaPatterns : MonoBehaviour
 {
+    [Header("Ref:")] 
+    [SerializeField] private Collider2D[] platforms;
+    [SerializeField] private LayerMask playerHitLayer;
+
+    private PlayerHpSystem playerHpSystem;
+    
+    
     [Header("General")]
-    [SerializeField] private int attackCounter = 0;
-    [SerializeField]private float[] attackDuration;
-    [SerializeField] private float stopTime = 0f;
+    [SerializeField] private int attackCounter = 0; //remove later
+    [SerializeField]private float[] attackDuration;     
+    [SerializeField] private float stopTime = 0f;     //remove later  
+    
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] [Range(0f, 10f)] private float yDistToStartJumping = 1f;
+    [SerializeField] private float timeBetweenJumps = 3f;
+    
+    private float nextJumpTime = 0f;
+
 
     [SerializeField] [Range(0f, 10f)] private float xDistToStartAttack = 1f;
 
@@ -14,19 +29,31 @@ public class MiyaPatterns : MonoBehaviour
     private Vector2 playerPos;
 
     private Rigidbody2D rb;
+    private Enemy_Flipped enemyFlipped;
+    private Collider2D col;
 
     [Header("Attack 2")] 
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
-    [SerializeField] [Range(0f, 10f)] private float yDistToStartJumping = 1f;
+    [SerializeField] private int damageToPlayer = 1;
+    [SerializeField] private float attack2AreaOffset;
+    [SerializeField] private float attack2AreaSize;
 
-    private bool isJumping = false;
-    private bool settingToFalse = false;
+    [Header("Attack 3")] 
+    [SerializeField] private float dashDist = 1f;
+    [SerializeField] private float dashSpeed = .2f;
+    [SerializeField] private float timeBetweenDashes = 3f;
+    [SerializeField] private float dashDuration = 1f;
+    private float nextDashTime = 0f;
+    
+ 
+
+
     
     void Start()
     {
         playerMovement = GameObject.FindGameObjectWithTag("PlayerBody").GetComponent<PlayerMovement>();
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        enemyFlipped = GetComponent<Enemy_Flipped>();
         
         //Start Attack1
         stopTime = Time.time + attackDuration[0];
@@ -35,13 +62,12 @@ public class MiyaPatterns : MonoBehaviour
     
     void FixedUpdate()
     {
-        playerPos = playerMovement.GetPlayerPos();
-
-
-
-
+        enemyFlipped.LookAtPlayer();
+        SetIgnorePlatformCollisions();
         
-        switch (2)
+        playerPos = playerMovement.GetPlayerPos();
+        
+        switch (3)
         {
             case 1: 
                 Attack1();
@@ -74,23 +100,25 @@ public class MiyaPatterns : MonoBehaviour
     {
         if (!ReachedTarget(playerPos))
         {
-            MoveToTarget(playerPos);
+             MoveToTarget(playerPos);
             
             float diff = playerPos.y - transform.position.y ;
             if (diff > yDistToStartJumping)
             {
-                if (!isJumping)
+                if (Time.time > nextJumpTime)
                 {
                     Jump();
+                    nextJumpTime = Time.time + timeBetweenJumps;
                 }
             }
         }
         else
         {
             print("slashslash");
+            //play attack2 animation, add anim event to damage player
         }
 
-        
+   
         
         if (IsAttackDurationEnded())
         {
@@ -101,8 +129,20 @@ public class MiyaPatterns : MonoBehaviour
     //quick dash, leaving a trail that damages the player if not avoided
     private void Attack3()
     {
- 
         print("SWOOSSH");
+        
+       // Physics.OverlapCapsule()
+
+       if (Time.time > nextDashTime)
+       {
+           rb.velocity = new Vector2(0f, 0f);
+           Dash();
+           nextDashTime = Time.time + timeBetweenDashes;
+       }
+       else
+       {
+         // MoveToTarget(playerPos);
+       }
         
         if (IsAttackDurationEnded())
         {
@@ -160,23 +200,10 @@ public class MiyaPatterns : MonoBehaviour
 
     private void Jump()
     {
+        print("jumping");
         rb.AddForce(new Vector2(rb.velocity.x, jumpForce) * Time.fixedDeltaTime, ForceMode2D.Impulse);
-        
-        if (!settingToFalse)
-        {
-            settingToFalse = true;
-            StartCoroutine(SetJumpingToFalse());
-        }
     }
-
-    private IEnumerator SetJumpingToFalse()
-    {
-        yield return new WaitForSeconds(.5f);
-        isJumping = false;
-
-        settingToFalse = false;
-    }
-
+    
     private bool ReachedTarget(Vector2 targetPos)
     {
         if (Vector2.Distance(targetPos, transform.position) > xDistToStartAttack)
@@ -185,5 +212,84 @@ public class MiyaPatterns : MonoBehaviour
         }
         
         return true;
+    }
+
+    private void SetIgnorePlatformCollisions()
+    {
+        if (rb.velocity.y > 0f)
+        {
+            IgnorePlatformCollisions(true);
+        }
+        else
+        {
+            IgnorePlatformCollisions(false);
+        }
+    }
+
+    //for when miya is jumping, to avoid collision with the floating platforms
+    private void IgnorePlatformCollisions(bool status)
+    {
+        foreach (Collider2D c in platforms)
+        {
+            Physics2D.IgnoreCollision(c, col, status);
+        }
+    }
+
+    public void Attack2DamagePlayer()
+    {
+        Collider2D hitPlayer;
+
+        if (!enemyFlipped.GetIsFacingRight())
+        {
+            hitPlayer = Physics2D.OverlapCircle(transform.position - new Vector3(attack2AreaOffset, 0f, 0f ), attack2AreaSize, playerHitLayer);
+        }
+        else
+        {
+            hitPlayer = Physics2D.OverlapCircle(transform.position - new Vector3(-attack2AreaOffset, 0f, 0f ), attack2AreaSize, playerHitLayer);
+        }
+        
+        if (hitPlayer != null)
+        {
+            Transform playerRoot = hitPlayer.transform.root;
+            
+            //If no ref already
+            if (playerHpSystem == null)
+            {
+                //Locate PlayerBody to get hp system script
+                playerHpSystem = playerRoot.GetChild(0).GetComponent<PlayerHpSystem>();
+            }
+            playerHpSystem.TakeDamage(damageToPlayer);
+        }
+    }
+
+    private void Dash()
+    {
+        float dirX = 1f;
+        if (!enemyFlipped.GetIsFacingRight())
+        {
+            dirX *= -1;
+        }
+
+       
+        StartCoroutine(DashStop());
+        rb.velocity = new Vector2(dirX * dashSpeed, rb.velocity.y);
+    }
+
+    private IEnumerator DashStop()
+    {
+        //yield return new WaitForSeconds(dashDuration);
+
+        float destX = (transform.position.x + dashDist) * -transform.right.x;
+        float startX = transform.position.x;
+        while (transform.position.x < destX)
+        {
+            yield return null;
+        }
+        rb.velocity = new Vector2(0f, 0f);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position - new Vector3(attack2AreaOffset, 0f, 0f ), attack2AreaSize);
+        Gizmos.DrawSphere(transform.position - new Vector3(-attack2AreaOffset, 0f, 0f ), attack2AreaSize);
     }
 }
