561ae6c48abce73ea2047c0ea294c7bc53b59962
jebbajeb <85389455+jebbajeb@users.noreply.github.com>
2022-04-11 23:40:39 +0800

    videoplayer

---
+++ Agent Satu [GPS1_Project]/Assets/02 Scripts/VideoPlayer/SkipableVideo.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SkipableVideo : VideoPlaying
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("ProceedInteraction"))
+        {
+            EndIntro();
+        }
+    }
+}
---
+++ Agent Satu [GPS1_Project]/Assets/02 Scripts/VideoPlayer/VideoPlaying.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlaying : MonoBehaviour
+{
+    private VideoPlayer vp;
+    private TransitionScript transition;
+
+    private void OnDestroy()
+    {
+        vp.loopPointReached -= EndPointReached;
+    }
+
+
+    private void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+        transition = FindObjectOfType<TransitionScript>();
+        
+        vp.loopPointReached += EndPointReached;
+    }
+
+    private void EndPointReached(VideoPlayer vp)
+    {
+        EndIntro();
+    }
+
+    protected void EndIntro()
+    {
+        transition.ReturnToMainMenu();
+    }
+}
