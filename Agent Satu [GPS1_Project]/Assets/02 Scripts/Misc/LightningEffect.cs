e3de01d5f8c9e2518ddc5c7f51c29772426a8610
JakeCJC <98402268+JakeCJC@users.noreply.github.com>
2022-04-17 23:44:10 +0800

    made changes to lightning effect

--- Agent Satu [GPS1_Project]/Assets/02 Scripts/Misc/LightningEffect.cs
+++ Agent Satu [GPS1_Project]/Assets/02 Scripts/Misc/LightningEffect.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
@@ -6,30 +7,31 @@ public class LightningEffect : MonoBehaviour
     
     [SerializeField] private new Light2D light;
     [SerializeField] private float timer = 0.0f;
-    private bool lightningable;
-        
+    private bool lightningable = true;
+    private float[] lightningTimer;
+
     void Start()
     {
         //light = .GetComponent<UnityEngine.Rendering.Universal.Light2D>();
         if (timer == 0)
             timer = 1.0f;
         lightningable = true;
+
+        lightningTimer = new float[3];
+        lightningTimer[0] = 6.0f;
+        lightningTimer[1] = 8.0f;
+        lightningTimer[2] = 10.0f;
+
+        StartCoroutine(generateLightning());
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("k") && lightningable)
+        if (!lightningable)
         { 
-            lightning();
-            lightningable = false;
+            StartCoroutine(returnBackToNormal());
         }
-
-        if(timer<=0)
-        {
-            lightningable = true;
-            timer = 1.0f;
-        }
-        timer *= Time.deltaTime;
+        
     }
 
     void lightning()
@@ -37,6 +39,29 @@ public class LightningEffect : MonoBehaviour
         light.intensity = 1.0f;
     }
 
+    IEnumerator returnBackToNormal()
+    {
+        lightningable = true;
+
+        yield return new WaitForSeconds(timer);
+    }
+
+    IEnumerator generateLightning()
+    {
+        while (true)
+        {
+            int temp = Random.Range(0, 3);
+            //Debug.Log(lightningTimer[temp]);
+
+            if (lightningable)
+            {
+                lightning();
+                lightningable = false;
+            }
+            //Debug.Log("lightning triggered");
+            yield return new WaitForSeconds(lightningTimer[temp]);  
+        }
+    }
 }
 
 
